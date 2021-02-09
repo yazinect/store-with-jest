6b302831578e234eb2e440fdea081008c5c4af2e
Yazmin Pedraza <yazminpedraza.it@gmail.com>
2021-02-09 11:32:43 -0600

    Configuration of jest and enzyme

---
+++ src/__test__/components/Footer.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Footer from '../../components/Footer';
+
+describe('<Footer />', () => {
+  test('Render del componente Footer ', () => {
+    const footer = mount(<Footer />);
+    expect(footer.length).toEqual(1);
+  })
+  
+})
+
