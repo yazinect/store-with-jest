b650b3af56deec607f08451ea1da39988962abeb
Yazmin Pedraza <yazminpedraza.it@gmail.com>
2021-02-09 18:39:28 -0600

    Testing actions and reducers

---
+++ src/__test__/reducers/reducers.test.js
@@ -0,0 +1,41 @@
+import reducer from '../../reducers';
+import ProductMock from '../../__mocks__/ProductMock';
+
+describe('Reducers', () => {
+  test('Comprobar initial state', () => {
+    expect(reducer({}, '')).toEqual({});
+  });
+  test('ADD_TO_CART', () => {
+    const initialState = {
+      cart: [],
+    };
+    const payload = ProductMock;
+    const action = {
+      type: 'ADD_TO_CART',
+      payload,
+    };
+    const expected = {
+      cart: [
+        ProductMock,
+      ],
+    };
+    expect(reducer(initialState, action)).toEqual(expected);
+  });
+
+  test('REMOVE_TO_CART', () => {
+    const initialState = {
+      cart: [
+        ProductMock,
+      ],
+    };
+    const payload = ProductMock;
+    const action = {
+      type: 'REMOVE_FROM_CART',
+      payload,
+    };
+    const expected = {
+      cart: [],
+    };
+    expect(reducer(initialState, action)).toEqual(expected);
+  });
+});
