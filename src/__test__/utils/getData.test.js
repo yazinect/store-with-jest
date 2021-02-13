5e6e70fd547c66b7255afe5e7635042bf6eaf622
Yazmin Pedraza <yazminpedraza.it@gmail.com>
2021-02-13 00:29:14 -0600

    Add test for fetch

---
+++ src/__test__/utils/getData.test.js
@@ -0,0 +1,15 @@
+import getData from '../../utils/getData';
+
+describe('Fetching API', () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
+  test('Llamar una API y retornar datos', () => {
+    fetch.mockResponseOnce(JSON.stringify( { data: '12345' }));
+    getData('https://google.com')
+      .then( response => {
+        expect(response.data).toEqual('12345');
+      });
+  });
+});
